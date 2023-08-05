af9bb611ff7592281f9c9cf81de6fe5f103e271f
Elias Gonzalez <eliasivang@gmail.com>
2023-08-05 13:08:58 -0300

    fix-footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import {PiGithubLogo} from 'react-icons/pi'
 
 function Footer() {
   return (
-    <div className='absolute bottom-0 flex items-center justify-center w-full '>
+    <div className='absolute bottom-0 left-0 right-0 flex items-center justify-center w-full'>
             <p className='p-3 text-center text-white text-md md:text-xl font-extralight'>Creado por <span className='p-1 hover:bg-principal'><a target="_blank" href="https://github.com/Eliasivang">Elias Gonzalez</a></span></p>
             <PiGithubLogo color='white' className='text-lg md:text-2xl'/>
     </div>
