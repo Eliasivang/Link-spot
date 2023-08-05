03016479963696ed638b4003350c99c2657a7d8e
Elias Gonzalez <eliasivang@gmail.com>
2023-08-05 14:16:08 -0300

    text

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ import {PiGithubLogo} from 'react-icons/pi'
 function Footer() {
   return (
     
-    <div className='absolute bottom-0 left-0 right-0 flex items-center justify-center w-full sm:relative '>
+    <div className='relative bottom-0 left-0 right-0 flex items-center justify-center w-full sm:absolute '>
             <p className='p-3 text-center text-white text-md md:text-xl font-extralight'>Creado por <span className='p-1 hover:bg-principal'><a target="_blank" href="https://github.com/Eliasivang">Elias Gonzalez</a></span></p>
             <PiGithubLogo color='white' className='text-lg md:text-2xl'/>
     </div>
