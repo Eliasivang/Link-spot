762b051dc08627d7e74dcc2d9d80092b5ea5b00e
Elias Gonzalez <eliasivang@gmail.com>
2023-08-05 18:10:41 -0300

    testv

--- src/app/user/[username]/page.jsx
+++ src/app/user/[username]/page.jsx
@@ -10,7 +10,7 @@ function PublicPage() {
     const params = useParams();
     const [profile,setProfile] = useState(null);
     const [url,setUrl] = useState('');
-    const [state,setState] = useState(0)
+    const [state,setState] = useState(0);
     const router = useRouter();
     function onHandleCreateAccount(){
         router.push('/')
@@ -28,13 +28,15 @@ function PublicPage() {
                     console.log(profile)
                     const url = await getProfilePhotoUrl(userInfo.profileInfo.profilePicture);                  
                     setUrl(url);
-                    setState(6)
+                    setState(6);
+                    console.log(url)
                 }else{setState(7)}
             }catch (error) {
                 console.error(error);
             }
         }
     },[]);
+    
     if(state===0){
         return (
             <div className='flex items-center justify-center w-full h-screen '>
@@ -57,9 +59,15 @@ function PublicPage() {
                     <button onClick={onHandleCreateAccount} className='p-2 my-2 text-sm text-white rounded hover:bg-violet-700 md:text-md md:my-4 bg-principal'>Crear una cuenta</button>
                 </div>
                 
+                {url == undefined ?
+                <div className='w-[150px] h-[150px]  rounded-full overflow-hidden my-2 lg:my-6'>
+                    <img src='https://i.pinimg.com/236x/af/de/72/afde727d75f5aa585c407cd89910cb80.jpg' className='w-[150px] h-[150px] text-center'  alt='profile_photo'/>
+                </div>
+                :
                 <div className='w-[150px] h-[150px]  rounded-full overflow-hidden my-0 lg:my-6'>
-                    <img src={url} className='w-[150px] h-[150px] text-center'  alt='profile_photo'/>
-                </div>                
+                <img src={url} className='w-[150px] h-[150px] text-center'  alt='profile_photo'/>
+                </div> 
+                }        
                 <div className='flex flex-col items-center justify-center'>
                     <h2 className='text-2xl font-semibold text-white'>@{profile?.profileInfo.username}</h2>
                     <h3 className='text-xl text-white'>{profile?.profileInfo.displayName}</h3> 
